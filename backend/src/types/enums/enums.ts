85137a9cdd93767f88f28e515a50a33dcc5ad1ff
shivakrishnak13 <kosarishivakrishna10@gmail.com>
2026-08-22 13:15:39 +0530

    feat: add MongoDB application models

--- backend/src/types/enums/enums.ts
+++ backend/src/types/enums/enums.ts
@@ -6,3 +6,14 @@ export enum ENV {
   PRODUCTION = "production",
   DEVELOPMENT = "development",
 }
+
+export enum APPLICATION_STATUS {
+  DRAFT = "DRAFT",
+  SUBMITTED = "SUBMITTED",
+  COMPLETED = "COMPLETED",
+}
+
+export enum DOCUMENT_TYPE {
+  ID_PROOF = "ID_PROOF",
+  DEGREE_CERTIFICATE = "DEGREE_CERTIFICATE",
+};

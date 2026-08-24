592f441fafae7a7cb649f6203a64eda32a783719
shivakrishnak13 <kosarishivakrishna10@gmail.com>
2026-08-24 22:02:11 +0530

    feat: setup env config / routes / axios

---
+++ frontend/config/routes.ts
@@ -0,0 +1,6 @@
+export const routes = {
+  auth: {
+    signUp: "/signup",
+    signIn: "/signin",
+  },
+};
---
+++ frontend/lib/api.ts
@@ -0,0 +1,38 @@
+import { env } from "@/env.mjs";
+import axios, { type AxiosError } from "axios";
+
+const apiUrl = env.NEXT_PUBLIC_API_URL;
+
+const HEADERS = {
+  "Content-Type": "application/json",
+};
+
+const apiClient = axios.create({
+  baseURL: apiUrl,
+  headers: {
+    ...HEADERS,
+  },
+  withCredentials: true,
+});
+
+export type ErrorResponseType = {
+  message: string;
+};
+
+apiClient.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error: AxiosError<ErrorResponseType>) => {
+    const status = error?.response?.status;
+    const errorMessage = error?.response?.data?.message?.toLowerCase() || "";
+
+    if (status === 401 && errorMessage.includes("invalid token")) {
+      window.location.href = "/signin";
+    }
+
+    return Promise.reject(error);
+  },
+);
+
+export { apiClient, apiUrl };
